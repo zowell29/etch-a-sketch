f91131060e3f19285626242f89057e3e18c3af4e
Zowell <67911888+zowell29@users.noreply.github.com>
2024-04-03 20:33:30 +0300

    remove unwanted console.log

--- javascript.js
+++ javascript.js
@@ -62,7 +62,7 @@ function addListenerOnHover() {
         divMatrixElement.addEventListener('mouseover', () => {
             let randomNumber = Math.floor(Math.random()*16777215).toString(16);
             let randomColor = "#" + randomNumber;
-            console.log(randomColor);
+            // console.log(randomColor);
             divMatrixElement.style.backgroundColor = randomColor;
         })  
     });
